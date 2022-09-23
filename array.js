598828d7498f9fe7b46eab78f456788b409bfd45
HasanurHasu <hasanurhasu127@gmail.com>
2022-09-23 12:00:28 +0600

    frist push

---
+++ array.js
@@ -0,0 +1,4 @@
+var friendsAge = [25, 30, 36, 48];
+friendsAge[1] = 45;
+var position = friendsAge.indexOf(48);
+console.log(friendsAge);
---
+++ hello.js
@@ -0,0 +1,28 @@
+var number = 55;
+if (number >= 80) {
+    console.log("Your grade is: A+")
+}
+else if(number >= 70) {
+    console.log("Your grade is: A")
+}
+else if (number >= 60) {
+    console.log("Your grade is: A-")
+}
+else if (number >= 50) {
+    console.log("Your grade is: B")
+}
+else if (number >= 40) {
+    console.log("Your grade is: C")
+}
+else if (number >= 33) {
+    console.log("Your grade is: D")
+}
+else if (number >= 0) {
+    console.log("Your grade is: F")
+}
+else {
+    console.log("Wrong Input")
+}
+
+var date = new Date("1997-10-25");
+console.log(date);
