ad5d8e1dfa204f1aac281ab9f0f6b3a4fd1e80d9
HasanurHasu <hasanurhasu127@gmail.com>
2022-09-24 20:06:41 +0600

    js

--- array.js
+++ array.js
@@ -3,19 +3,33 @@ friendsAge[1] = 45;
 friendsAge[friendsAge.length] = 11;
 friendsAge.push(25);
 friendsAge.push(28);
-console.log(friendsAge);
+// console.log(friendsAge);
 friendsAge.pop();
-console.log(friendsAge);
+// console.log(friendsAge);
+
 var importentParson = [];
 importentParson.push("Hasanur", "Shahinur");
-console.log(importentParson);
+// console.log(importentParson);
 importentParson.shift();
 importentParson.unshift("Eleyas", "Dukhu", "Mojhahar");
-console.log(importentParson);
+// console.log(importentParson);
+
 var tealine = ["Hasanur", "Shahinur", "Khademul", "Lutfor,", "Foysal", "Eyakub", "Eliyas", "Dukhu"];
 var line = tealine.slice(3);
-console.log(tealine);
-console.log(line);
+// console.log(tealine);
+// console.log(line);
 
-var color = 1;
-for 
+var color = "blue";
+switch (color) {
+    case "blue":
+        console.log("Your selected color is blue");
+        break;
+    case "green":
+        console.log("Your selected color is green.");
+        break;
+    case "white":
+        console.log("Your selected color is white.");
+        break;
+    default:
+        console.log("Wrong selected.");
+}
