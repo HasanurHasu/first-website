c9d06ba34dcc3e64218cd70d4a19b81447489957
HasanurHasu <hasanurhasu127@gmail.com>
2022-09-25 00:24:43 +0600

    switch js added

---
+++ switch.js
@@ -0,0 +1,14 @@
+var color = "red";
+switch (color) {
+    case "blue":
+        console.log("Your selected color is blue.");
+        break;
+    case "green":
+        console.log("Your selected color is green.");
+        break;
+    case "red":
+        console.log("Your selected color is red.");
+        break;
+    default:
+        console.log("No color is selected.");
+}
