08179b74bad232fd57be28d8230dc6d6f4e26fe4
HasanurHasu <hasanurhasu127@gmail.com>
2022-09-25 00:38:23 +0600

    function

---
+++ function.js
@@ -0,0 +1,8 @@
+function sleep(name, time) {
+    console.log("Everyday "+ name +" is sleeping at " + time);
+}
+sleep("Hasanur", "1 AM");
+sleep("Shahinur", "12 AM");
+sleep("Rohim", "11 PM");
+sleep("Korim", "10 PM");
+sleep("Pagla", "9 PM");
