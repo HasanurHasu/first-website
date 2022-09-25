312c33851fe2236a37763da226453137822a183b
HasanurHasu <hasanurhasu127@gmail.com>
2022-09-25 13:07:12 +0600

    inchToFeet

--- inchToFeet.js
+++ inchToFeet.js
@@ -1,15 +1,30 @@
-function inchToFeet (inch) {
-    var feet = inch/12;
-    return feet;
+// function inchToFeet (inch) {
+//     var feet = inch/12;
+//     return feet;
+// }
+
+// // var hasanur = inchToFeet(300);
+// // console.log(hasanur);
+// // var melu = inchToFeet(36);
+// // console.log(melu);
+
+// var inch1 = [12, 24, 36, 48, 60, 72, 146];
+// for (i = 0; i < inch1.length; i++) {
+//     var fee1 = inch1[i]/12;
+//     console.log(fee1);
+// }
+// const year = 2022;
+// const leapYear = year % 4;
+// console.log(leapYear == 0);
+
+function isLeapYear(year) {
+const leapYear = year % 4;
+if (leapYear == 0) {
+    return true;
+}
+else {
+    return false;
+}
 }
 
-// var hasanur = inchToFeet(300);
-// console.log(hasanur);
-// var melu = inchToFeet(36);
-// console.log(melu);
-
-var inch1 = [12, 24, 36, 48, 60, 72, 146];
-for (i = 0; i < inch1.length; i++) {
-    var fee1 = inch1[i]/12;
-    console.log(fee1);
-}
+console.log(isLeapYear(2024));
