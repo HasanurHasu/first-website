dab80d25cdbce50a9c2354da9cbabc204d4f6bb2
HasanurHasu <hasanurhasu127@gmail.com>
2022-09-26 17:15:49 +0600

    factorial

--- function.js
+++ function.js
@@ -1,8 +1,29 @@
-function sleep(name, time) {
-    console.log("Everyday "+ name +" is sleeping at " + time);
+// function sleep(name, time) {
+//     console.log("Everyday "+ name +" is sleeping at " + time);
+// }
+// sleep("Hasanur", "1 AM");
+// sleep("Shahinur", "12 AM");
+// sleep("Rohim", "11 PM");
+// sleep("Korim", "10 PM");
+// sleep("Pagla", "9 PM");
+
+
+
+// var i = 1;
+// factorial = 1;
+// while (i <= 20) {
+//     factorial = factorial * i;
+//     console.log(factorial);
+//     i++;
+// }
+
+function factorial(num) {
+    var i = 1;
+factorial = 1;
+while (i <= num) {
+    factorial = factorial * i;
+    i++;
 }
-sleep("Hasanur", "1 AM");
-sleep("Shahinur", "12 AM");
-sleep("Rohim", "11 PM");
-sleep("Korim", "10 PM");
-sleep("Pagla", "9 PM");
+    return factorial;
+}
+console.log(factorial(5));
